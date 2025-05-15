52da8fb4615487a804e38233538de15a0647f91c
Hansa Wickramanayake <hansaanuradha93@gmail.com>
2025-05-15 20:03:13 +0530

    Initial Commit

---
+++ Tiktok/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tiktok
+//
+//  Created by Hansa Wickramanayake on 2025-05-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tiktok/TiktokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiktokApp.swift
+//  Tiktok
+//
+//  Created by Hansa Wickramanayake on 2025-05-15.
+//
+
+import SwiftUI
+
+@main
+struct TiktokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
