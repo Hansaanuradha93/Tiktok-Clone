166386a2ef9fc4afa31e2bb6d55f711db910d7ee
Hansa Wickramanayake <hansaanuradha93@gmail.com>
2025-05-18 21:09:21 +0530

    feed cell design

--- Tiktok/Core/Feed/FeedCell.swift
+++ Tiktok/Core/Feed/FeedCell.swift
@@ -93,6 +93,7 @@ struct FeedCell: View {
                         }
                     }
                 }
+                .padding(.bottom, 80)
             }
             .padding()
         }
