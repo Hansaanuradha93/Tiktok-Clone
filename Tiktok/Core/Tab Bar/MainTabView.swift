90c85484218ba4ce99d144469ebb39c7b8ba8948
Hansa Wickramanayake <hansaanuradha93@gmail.com>
2025-05-18 20:37:17 +0530

    tab view design

---
+++ Tiktok/Core/Tab Bar/MainTabView.swift
@@ -0,0 +1,71 @@
+//
+//  MainTabView.swift
+//  Tiktok
+//
+//  Created by Hansa Wickramanayake on 2025-05-15.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    
+    @State private var selectedTab: Int = 0
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Text("Feed")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "house")
+                            .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                        Text("Home")
+                    }
+                }
+                .onAppear { selectedTab = 0 }
+                .tag(0)
+            
+            Text("Friends")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "person.2")
+                            .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+                        Text("Friends")
+                    }
+                }
+                .onAppear { selectedTab = 1 }
+                .tag(1)
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "heart")
+                            .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                        Text("Inbox")
+                    }
+                }
+                .onAppear { selectedTab = 3 }
+                .tag(3)
+            
+            Text("Profile")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "person")
+                            .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
+                        Text("Profile")
+                    }
+                }
+                .onAppear { selectedTab = 4 }
+                .tag(4)
+        }
+        .tint(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
